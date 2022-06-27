c1fd60fe415e757378f0082a50e061e055569d29
Justin Liman <justinliman97@gmail.com>
2022-06-26 20:34:21 -0700

    import fs

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.readdir('.', (err, filenames) => {
+    if (err) {
+        // error handling code
+        console.log(err);
+    }
+    console.log(filenames);
+});
