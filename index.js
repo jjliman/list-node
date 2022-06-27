509e7c1c106fb45334f11450fb77851005d17545
Justin Liman <justinliman97@gmail.com>
2022-06-27 02:09:19 -0700

    implement promise based solution to file listing

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 
-fs.readdir('.', (err, filenames) => {
+const { lstat } = fs.promises;
+// const process = require('process');
+
+fs.readdir(process.cwd(), async (err, filenames) => {
     if (err) {
         // error handling code
         console.log(err);
     }
-    console.log(filenames);
+    
+    const statPromises = filenames.map((filename) => {return lstat(filename)});
+    const allStats = await Promise.all(statPromises);
+    
+    allStats.forEach((stats, idx) => {
+        console.log(filenames[idx], stats.isFile());
+    });
+
 });
